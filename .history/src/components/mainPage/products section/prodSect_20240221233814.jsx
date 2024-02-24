9a3597bfe8cc73a1b4ea854b577bfa4b903f7f32
Azizbek <azizbekkarimov@alatoo.edu.kg>
2024-02-25 02:29:29 +0600

    ProdSect section done

---
+++ .history/src/components/mainPage/products section/prodSect_20240221233814.jsx
@@ -0,0 +1,54 @@
+import React from 'react'
+import data from '../../.././data.json';
+import share from "../../../assets/icons/share.png"
+import compare from "../../../assets/icons/share.png"
+import share from "../../../assets/icons/share.png"
+const ProdSect = () => {
+  console.log(data.products)
+  const products = data.products.slice(0, 8);
+
+  return (
+    <div className='prodSec container'>
+      <h2>Our Products</h2>
+      <div className="products" style={{ display: 'flex', flexWrap: 'wrap' }}>
+        {products.map(product => (
+            <div key={product.name} style={{ width: '20%' }} className="product">
+              <img src={product.image} alt={product.name} />
+              <div className="proDec container">
+                <div className="proName">{product.name}</div>
+                <div className="proDesc">{product.description}</div>
+                <div className="proPrice">${product.price} <div className="oldPrice">${product.oldPrice}</div></div>
+              </div>
+              <div className="layout"></div>
+
+              <div class="hover_options">
+                  <div class="to_card_btn">
+                    <button>Add to card</button>
+                  </div>
+                  <div class="mut_options">
+                    <div class="share optn">
+                      {/* <img src="./img/share.png" alt=""> */}
+                      <img src="" alt="" />
+                        Share</div>
+                    <div class="compare optn">
+                      {/* <img src="./img/compare.png" alt=""> */}
+                        Compare</div>
+                    <div class="like optn">
+                      {/* <img src="./img/Like icon.png" alt=""> */}
+                        Like</div>
+                  </div>
+                </div>
+
+            </div>
+            
+            ))}
+      </div>
+      <div className="show_more">
+        <button>Show More</button>
+      </div>
+      
+    </div>
+  )
+}
+
+export default ProdSect;
